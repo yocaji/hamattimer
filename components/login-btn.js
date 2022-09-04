895426c5f9bc4a184da25b1e6e99095337b299c2
yocajii <yocajii@stone.sakura.ne.jp>
2022-09-04 12:50:25 +0900

    アクセストークン取得処理を追加した

--- components/login-btn.js
+++ components/login-btn.js
@@ -3,10 +3,12 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function LoginBtn() {
   const { data: session } = useSession()
   if (session) {
+    console.log(session.user)
     return (
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        <div>Access Token: {session.accessToken}</div>
       </>
     )
   }
