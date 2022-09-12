9338129ac935a3b6ff326d3d8169f698374fd181
yocajii <yocajii@stone.sakura.ne.jp>
2022-09-12 19:19:33 +0900

    GitHub連携前はエクスポートボタンが表示されないようにした

--- src/components/ExportBtn.js
+++ src/components/ExportBtn.js
@@ -12,20 +12,22 @@ export default function ExportBtn() {
     const content = Buffer.from(markdown).toString('base64')
     const octokit = new Octokit({ auth: session.accessToken })
     octokit.rest.repos
-      .createOrUpdateFileContents({
-        owner: session.user.name,
-        repo: 'playground',
-        path: `hamattimer/${Date.now()}.md`,
-        message: 'Uploaded by Hamattimer',
-        content: content,
-      })
-      .then(console.log(markdown))
+    .createOrUpdateFileContents({
+      owner: session.user.name,
+      repo: 'playground',
+      path: `hamattimer/${Date.now()}.md`,
+      message: 'Uploaded by Hamattimer',
+      content: content,
+    })
+    .then(console.log(markdown))
   }
 
-  return (
+  if (session) {
+    return (
     <a onClick={uploadFile} className={'navbar-item'}>
-      <GoMarkGithub className={'mr-2'} />
+      <GoMarkGithub className={'mr-2'}/>
       エクスポート
     </a>
-  )
+    )
+  }
 }
