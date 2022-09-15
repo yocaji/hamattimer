0b2514dcc0394fc5b7e06089a98d8d69c2be7ea7
yocajii <yocajii@stone.sakura.ne.jp>
2022-09-15 10:42:20 +0900

    GFMの改行ルールが使えるようにした

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,9 +1,11 @@
 import { useContext, useEffect } from 'react'
 import { MarkdownContext } from './providers/MarkdownProvider'
 import ReactMarkdown from 'react-markdown'
-import gfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm'
+import remarkBreaks from 'remark-breaks'
 
 export default function MarkdownPreview() {
+
   const { markdown, updateMarkdown } = useContext(MarkdownContext)
 
   useEffect(() => {
@@ -13,7 +15,7 @@ export default function MarkdownPreview() {
   return (
     <section className={'section'}>
       <p className={'is-size-7'}>Preview</p>
-      <ReactMarkdown remarkPlugins={[gfm]} unwrapDisallowed={false}>
+      <ReactMarkdown remarkPlugins={[remarkGfm, remarkBreaks]} unwrapDisallowed={false}>
         {markdown}
       </ReactMarkdown>
     </section>
