e502821fbb6346d3d6485734f933da0e4d14181f
yocajii <yocajii@stone.sakura.ne.jp>
2022-09-20 16:06:35 +0900

    フッターのコンポーネント設計を改善した

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,12 @@
-import { useSession, signOut } from 'next-auth/react'
+import FooterLinks from './modules/FooterLinks'
 
 export default function Footer() {
 
-  const { data: session } = useSession()
-
-  if (session) {
-    return (
-      <footer className={'footer'}>
-        <div className={'content has-text-centered'}>
-          <p className={'is-size-7'}>
-            <a onClick={() => signOut()}>GitHub連携解除</a>
-          </p>
-        </div>
-      </footer>
-    )
-  }
+  return (
+    <footer className={'footer'}>
+      <div className={'content has-text-centered'}>
+        <FooterLinks/>
+      </div>
+    </footer>
+  )
 }
