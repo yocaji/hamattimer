1e7b10d97ad8c400ae0538e8faac7386d17f2713
yocajii <yocajii@stone.sakura.ne.jp>
2022-09-28 07:49:49 +0900

    スタートボタンのテストを作成した

--- __tests__/e2e/timer.spec.js
+++ __tests__/e2e/timer.spec.js
@@ -19,6 +19,15 @@ test('初回アクセス', async ({ page }) => {
 
 test('スタートボタン', async ({ page }) => {
 	await page.goto('/timer')
-	const title = await page.innerText('h1')
-	await expect(title).toBe('はまったいまー')
+
+	await page.locator('_react=StartButton').click()
+	await expect(page.locator('_react=ControlButton')).toBeEnabled()
+	await expect(page.locator('#stopwatch-counter')).toHaveText('0:00:01')
+	await expect(page.locator('_react=Trials')).toBeVisible()
+	await expect(page.locator('_react=Trial >> textarea[name=guess]')).toHaveValue('')
+	await expect(page.locator('_react=Trial >> textarea[name=operation]')).toHaveValue('')
+	await expect(page.locator('_react=Trial >> textarea[name=result]')).toHaveValue('')
+	await expect(page.locator('_react=EditorFooter >> text=解決した！')).toBeVisible()
+	await expect(page.locator('_react=EditorFooter >> text=終了する')).toBeVisible()
+	await expect(page.locator('_react=Preview >> h2')).toHaveText(['解決したいこと', '試したこと その1'])
 })
