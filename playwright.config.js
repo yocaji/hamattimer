35909f159a3889917352fc9721329ae54526ffe3
yocajii <yocajii@stone.sakura.ne.jp>
2022-09-30 11:47:06 +0900

    storageStateを作成する処理を追加した

--- playwright.config.js
+++ playwright.config.js
@@ -2,14 +2,12 @@ import { devices } from '@playwright/test'
 
 const config = {
   testMatch: '/e2e/**/*.spec.js',
-  retries: process.env.CI ? 2 : 0,
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
     headless: true,
     ignoreHTTPSErrors: true,
     actionTimeout: 10_000,
-    trace: 'on',
-    video: 'on',
+    screenshot: 'only-on-failure',
   },
   projects: [
     {
