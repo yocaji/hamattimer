2c6ad77140ce3503ff3d4e6206299daf3d97b445
yocajii <yocajii@stone.sakura.ne.jp>
2022-10-04 15:26:05 +0900

    Selectのテストを作成した

--- src/components/atoms/Select.jsx
+++ src/components/atoms/Select.jsx
@@ -1,9 +1,9 @@
 export default function Select({ options, value, onChange, classNames }) {
   return (
     <span className={`select ${classNames}`}>
-      <select value={value} onChange={(e) => onChange(e.target.value)}>
+      <select value={value} onChange={(e) => onChange(e.target.value)} data-testid={'select'}>
         {options.map((option) => (
-          <option key={option.id}>{option.value}</option>
+          <option key={option.id} data-testid={'select-option'}>{option.value}</option>
         ))}
       </select>
     </span>
