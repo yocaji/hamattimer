b078f4d69231f9551128b52a9e28083b30d881fc
yocajii <yocajii@stone.sakura.ne.jp>
2022-10-06 09:27:29 +0900

    ストップウォッチボタンのテストを作成した

--- src/components/molecules/ButtonStopwatch.jsx
+++ src/components/molecules/ButtonStopwatch.jsx
@@ -1,13 +1,7 @@
-import { useContext } from 'react'
-import { IsStartedContext } from '../providers/IsStartedProvider'
 import { MdPause, MdPlayArrow } from 'react-icons/md'
 import Button from '../atoms/Button'
 
-export default function ButtonStopwatch(props) {
-
-  const { isStarted } = useContext(IsStartedContext)
-
-  const { start, pause, isRunning } = props.stopwatch
+export default function ButtonStopwatch({ isStarted, start, pause, isRunning }) {
 
   const action = () => {
     isRunning ? pause() : start()
--- src/components/organizations/Stopwatch.jsx
+++ src/components/organizations/Stopwatch.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useContext, useState, useEffect } from 'react'
+import { IsStartedContext } from '../providers/IsStartedProvider'
 import ButtonStopwatch from '../molecules/ButtonStopwatch'
 import SelectLimit from '../molecules/SelectLimit'
 import StopwatchCounter from '../organisms/StopwatchCounter'
 
 export default function Stopwatch({ stopwatch }) {
 
+  const { start, pause, isRunning } = stopwatch
+  const { isStarted } = useContext(IsStartedContext)
   const [limit, setLimit] = useState(30)
 
   useEffect(() => {
@@ -17,7 +20,7 @@ export default function Stopwatch({ stopwatch }) {
 
   return (
     <>
-      <ButtonStopwatch stopwatch={stopwatch}/>
+      <ButtonStopwatch isStarted={isStarted} start={start} pause={pause} isRunning={isRunning}/>
       <StopwatchCounter stopwatch={stopwatch} limit={limit}/>
       <SelectLimit limit={limit} setLimit={setLimit}/>
     </>
