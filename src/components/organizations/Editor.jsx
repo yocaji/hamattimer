7aedec0d69350728cff517c47cc80b128d6e97cb
yocajii <yocajii@stone.sakura.ne.jp>
2022-10-11 09:59:47 +0900

    Issueの見出しの位置をTrialsに合わせた

--- src/components/organizations/Editor.jsx
+++ src/components/organizations/Editor.jsx
@@ -2,13 +2,12 @@ import { FormProvider } from 'react-hook-form'
 import Issue from '../organisms/Issue'
 import Trials from '../organisms/Trials'
 
-export default function Editor(props) {
+export default function Editor({ methods }) {
 
   return (
     <>
       <div className={'tile is-child'}>
-        <h2 className={'title is-5 mt-4'}>解決したいこと</h2>
-        <FormProvider {...props.methods}>
+        <FormProvider {...methods}>
           <Issue/>
         </FormProvider>
       </div>
