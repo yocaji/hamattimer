599512509973d1d78108dd09848640b436559472
yocajii <yocajii@stone.sakura.ne.jp>
2022-10-11 15:39:45 +0900

    コピー/保存ボタンをPreviewコンポーネントに移動した

--- src/components/organizations/Preview.jsx
+++ src/components/organizations/Preview.jsx
@@ -3,7 +3,8 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import remarkBreaks from 'remark-breaks'
 import { MarkdownContext } from '../providers/MarkdownProvider'
-import PreviewHead from './PreviewHead'
+import ButtonGist from '../molecules/ButtonGist'
+import ButtonCopy from '../molecules/ButtonCopy'
 
 export default function Preview() {
 
@@ -15,8 +16,15 @@ export default function Preview() {
 
   return (
     <>
-      <div className={'mt-2 mb-5'}>
-        <PreviewHead/>
+      <div className={'columns is-variable is-1 pt-2'}>
+        <div className={'column is-6'}>
+        </div>
+        <div className={'column is-3'}>
+          <ButtonCopy/>
+        </div>
+        <div className={'column is-3'}>
+          <ButtonGist/>
+        </div>
       </div>
       <div className={'tile is-child box'} id={'preview'}>
         <div className={'content'}>
