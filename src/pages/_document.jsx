7129d89da1d420ec7530b2ee0ec5c57b973aa4cb
yocajii <yocajii@stone.sakura.ne.jp>
2022-10-12 13:19:48 +0900

    Twitter cardを設定した

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -18,6 +18,10 @@ class MyDocument extends Document {
           <meta property="og:description" content="はまったいまーはプログラミング学習者のための問題解決サポートツールです。時間に区切りを作るためのタイマー機能と、思考を整理するためのメモ機能で、効率的に問題解決に取り組むお手伝いをします。"/>
           <meta property="og:site_name" content="はまったいまー"/>
           <meta property="og:image" content="https://hamattimer.app/og-image.png"/>
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:site" content="@yocajii"/>
+          <meta name="twitter:domain" content="hamattimer.app"/>
+          <meta name="twitter:image" content="https://hamattimer.app/og-twitter.png"/>
         </Head>
         <body>
         <Main/>
