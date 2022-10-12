608c4d3ae8a734bf01fa6f370cfcc7714c32ae5d
yocajii <yocajii@stone.sakura.ne.jp>
2022-10-12 20:49:37 +0900

    時間選択リストのユニットテストを修正した

--- __tests__/jest/molecules/SelectLimit.spec.jsx
+++ __tests__/jest/molecules/SelectLimit.spec.jsx
@@ -2,14 +2,13 @@ import { render, screen, fireEvent, within } from '@testing-library/react'
 import SelectLimit from '../../../src/components/molecules/SelectLimit'
 
 describe('SelectLimit', () => {
-  //
   let renderResult
-  let mockFunction
+  let setLimit
 
   beforeEach(() => {
-    mockFunction = jest.fn()
+    setLimit = jest.fn()
     renderResult = render(
-      <SelectLimit limit={30} setLimit={mockFunction}/>,
+      <SelectLimit limit={30} setLimit={setLimit}/>,
     )
   })
 
@@ -19,12 +18,12 @@ describe('SelectLimit', () => {
 
   test('limitの値が選択済みとなっている', async () => {
     const elements = await screen.getAllByTestId('select-option')
-    expect(elements[1].selected).toBe(true)
+    expect(elements[2].selected).toBe(true)
   })
 
   test('値を変更したらprops.setLimitで渡した関数が呼ばれる', async () => {
-    const desktopSection = await screen.getByTestId('select-limit-desktop')
+    const desktopSection = await screen.getByTestId('select-limit')
     fireEvent.change(within(desktopSection).getByRole('combobox'), { target: { value: 60 } })
-    expect(mockFunction).toHaveBeenCalledTimes(1)
+    expect(setLimit).toHaveBeenCalledTimes(1)
   })
 })
