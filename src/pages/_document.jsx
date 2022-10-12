efcb84ad6fcdc5113ca1aaf00cf1c0121c2669d7
yocajii <yocajii@stone.sakura.ne.jp>
2022-10-12 12:50:40 +0900

    OGPを設定した

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -12,6 +12,12 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400;700&family=Source+Code+Pro&family=Zen+Maru+Gothic:wght@500;700&display=swap"
             rel="stylesheet"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="はまったいまー｜プログラミングでハマる前に"/>
+          <meta property="og:url" content="https://hamattimer.app"/>
+          <meta property="og:description" content="はまったいまーはプログラミング学習者のための問題解決サポートツールです。時間に区切りを作るためのタイマー機能と、思考を整理するためのメモ機能で、効率的に問題解決に取り組むお手伝いをします。"/>
+          <meta property="og:site_name" content="はまったいまー"/>
+          <meta property="og:image" content="https://hamattimer.app/og-image.png"/>
         </Head>
         <body>
         <Main/>
